62116ba26727aef528f9d1245f2eac59ca2186d4
vanvuong1118 <73210556+vanvuong1118@users.noreply.github.com>
2022-02-11 15:18:15 +0700

    'video4'

---
+++ video4/video4.js
@@ -0,0 +1,12 @@
+//strings................................
+let a = ' mot string - chuoi';
+console.log(a);
+console.log(4 + 5 + 'a');// = 9 + 'a'
+
+console.log('a' + 4 + 5);// = a4 + 5 = a45
+//Js cong tu trai sang phai khong phan biet ro kieu du lieu...(so + so = so)(chuoi + so = chuoi)
+
+
+
+let b = '123456789'
+console.log(b.length)
