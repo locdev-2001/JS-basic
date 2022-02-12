092d0a0079f5fb1efe5b3d7b9d23c0c7083bc11e
vanvuong1118 <73210556+vanvuong1118@users.noreply.github.com>
2022-02-12 13:22:35 +0700

    Numbers

--- Numbers/numbers.js
+++ Numbers/numbers.js
@@ -1,2 +1,15 @@
 console.log('Hello world from HTML'.toUpperCase());
-//string Method and Properties
+//numbers
+//int float double long
+//không có khải niệm số thực, số nguyên trong JS
+let a = 5;
+let b = 6;
+let c = 6.9;
+let d = '5';
+console.log(a + b)
+console.log(a + c)
+console.log(a - d)
+let e = Number(d)// convert string to number
+//or
+let f = +d // can use + to convert string to number
+console.log('e: ', e, 'type : ', typeof e, ' f: ', f, 'type: ', typeof f)
